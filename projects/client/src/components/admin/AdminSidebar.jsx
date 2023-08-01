d6cd91475409873e57d379b0ee36aa8634bc6d53
gifariarsal <117580307+gifariarsal@users.noreply.github.com>
2023-08-01 11:14:42 +0700

    update sidebar

--- projects/client/src/components/admin/AdminSidebar.jsx
+++ projects/client/src/components/admin/AdminSidebar.jsx
@@ -10,7 +10,7 @@ import { Link } from "react-router-dom";
 const Sidebar = () => {
   return (
     <Box as={"aside"} w={"64"} bg={"#2C3E50"} color="white" minH="100vh">
-      <VStack spacing="4" align="stretch">
+      <VStack spacing="2" align="stretch">
         {/* Sidebar header */}
         <Box w={"full"} bg={"#D27321"} textAlign={"center"}>
           <Text fontSize="xl" fontWeight="bold" p="4">
@@ -18,11 +18,20 @@ const Sidebar = () => {
           </Text>
         </Box>
 
-        <Box>
-          <Text fontSize="lg" fontWeight="bold" p="2">
-            Product Management
-          </Text>
-        </Box>
+        <Link>
+          <Box
+            display={"flex"}
+            alignItems={"center"}
+            justifyContent={"center"}
+            p={"4"}
+            _hover={{ bg: "#4E586E" }}
+          >
+            <IoBagHandleOutline size={24} />
+            <Text fontSize="lg" fontWeight="bold" ml={2}>
+              Product Management
+            </Text>
+          </Box>
+        </Link>
         <Box>
           <Link to={"/about"}>
             <Text fontSize="lg" fontWeight="bold" p="2">
